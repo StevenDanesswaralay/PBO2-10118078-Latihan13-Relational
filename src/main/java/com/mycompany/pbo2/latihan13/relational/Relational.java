0202665be0617f85b0367f9aa736873b1f54fa52
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:25:18 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan13/relational/Relational.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Relational
+ */
+public class Relational {
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+}
